64ea7508d6d0359ce64080a47bee2a7719205c47
samuel-gonzalez <samuel.gonzalez@alumnos.upm.es>
2025-03-26 15:47:58 +0100

    primer commit prueba

--- tests/application/test_get_dice_roll.py
+++ tests/application/test_get_dice_roll.py
@@ -3,6 +3,6 @@ from app.application.get_dice_roll import get_dice_roll
 
 def test_result_is_a_valid_dice_value() -> None:
     for _ in range(100):
-        result = get_dice_roll()
+        result =  get_dice_roll()
         assert isinstance(result, int)
         assert result in {1, 2, 3, 4, 5, 6}
